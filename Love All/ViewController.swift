e9cfcd5bb97d93df8d0d83e7455cf780e5ec2a76
Christopher Kang <christopherkang@christophersair.home>
2019-01-05 13:51:12 -0500

    Initial Commit

---
+++ Love All/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Love All
+//
+//  Created by Christopher Kang on 1/5/19.
+//  Copyright © 2019 Christopher Kang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
